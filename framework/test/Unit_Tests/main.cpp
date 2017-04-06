199478ba43ef018c9a88a4f54831739e3d7acda2
CSol314 <cindysol@gmail.com>
2017-04-06 08:01:22 -0400

    Fixed spacing in main.cpp

--- framework/test/Unit_Tests/main.cpp
+++ framework/test/Unit_Tests/main.cpp
@@ -1,18 +1,13 @@
-
-#include <iostream>
-
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
 
 
 
-int main(int argc, char * argv[]) {
-	
-	
-	std::cout << "Hello, World!\n";
-	::testing::InitGoogleTest(&argc, argv);
-	
-	
-    return RUN_ALL_TESTS();
+int main(int argc, char * argv[])
+{
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
